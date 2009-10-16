3c4b226aa8b72fa6bddb4da01ebaf418000fb81e
seank <info@powerefi.com>
2009-10-16 13:32:39 -0700

    RPM only OutPut, almost ready

--- parser.c
+++ parser.c
@@ -49,12 +49,13 @@
 int addCharToString(char string[], char c);
 
 int main( int argc, char *argv[] ){
-	if(argc == 1){
-		puts("No argument supplied!! Please supply a file to parse.");
-	}else if(argc == 2){
+//	if(argc == 1){
+	if(argc == 5){
+	puts("No argument supplied!! Please supply a file to parse.");
+	}else if(argc == 1){  /* was 2 */
 		FILE *inputFile;
-		inputFile = fopen(argv[1], "rb");
-
+	//	inputFile = fopen(argv[1], "rb");
+        inputFile = fopen("slc.mountain.starting.near.top.bin","rb");
 		/* Could not open file */
 		if(inputFile == NULL){
 			printf("Error opening file %s, please check the name and try again!\n", argv[1]);
@@ -113,18 +114,19 @@ int main( int argc, char *argv[] ){
 					/* Increment the counter */
 					startsInsidePacket++;
 					/* TODO Move this to a function/add structure */
-					char RPMHiByte = 2;
-					char RPMLowByte = 5;
+					char RPMHiByte = 255;
+					char RPMLowByte = 255;
 					//char RPMHiByte = character + 28;
 					//char RPMLowByte = character + 29;
-					char RPMString[3];
-					RPMString[0] = RPMHiByte;
-					RPMString[1] = RPMLowByte;
-					RPMString[2] = END_OF_STRING;
-					printf("\n RPMString is -> %s",RPMString);
-					int  RPM = atoi(RPMString);
+				//	char RPMString[3];
+				//	RPMString[0] = RPMHiByte;
+				//	RPMString[1] = RPMLowByte;
+				//	RPMString[2] = END_OF_STRING;
+				//	printf("\n RPMString is -> %s",RPMString);
+				//	int  RPM = atoi(RPMString);
+					unsigned int RPM = RPMHiByte * 0xFF;
 					printf("\n RPM is -> %d",RPM);
-					printf("\n LowByte is -> %d",RPMLowByte);
+				//	printf("\n LowByte is -> %d",RPMLowByte);
 					printf("\n HighByte is -> %d",RPMHiByte);
 					fputc('t',outputFile);
 					char delimiter = '\n';
